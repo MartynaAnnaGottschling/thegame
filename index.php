9e50d25928609170944585e70458650a1606540c
ruda <ruda@example.com>
2020-11-26 11:35:09 +0100

    Add base login form in index.php file

--- index.php
+++ index.php
@@ -5,6 +5,16 @@
     <title>The Game</title>
   </head>
   <body>
+    <h1>Wikingowie i Słowianie</h1>
+    <h3 style="display:inline-block;margin:auto 2%;">Wybierz swój lud</h2>
+    <h3 style="display:inline-block;margin:auto 2%;">Zbuduj osadę</h3>
+    <h3 style="display:inline-block;margin:auto 2%;">Zbierz wojsko</h3>
+    <h2 style="display:inline-block;margin:auto 2%;">Przetrwaj!</h2>
 
+    <form class="" action="" method="post">
+      <p> Login: <input type="text" name="login" value=""> </p>
+      <p> Hasło: <input type="password" name="password" value=""> </p>
+      <input type="submit" value="Zaloguj">
+    </form>
   </body>
 </html>
