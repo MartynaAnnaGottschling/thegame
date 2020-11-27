43844a9f83c7ee82d0baf3b5b3dffcd93573479f
ruda <ruda@example.com>
2020-11-27 13:15:33 +0100

    Add credentials verification

---
+++ game.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>The Game</title>
+  </head>
+  <body>
+    <h1>Witaj użytkowniku</h1>
+  </body>
+</html>
--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
 
+  session_start();
+
   // file contains parametres for database connection
   require_once "connectdb.php";
   /*
@@ -16,12 +18,52 @@
 
   // if connection to database failed
   if ($connection->connect_errno!=0){
+    // show error code
     echo "Error: ".$connection->connect_errno;
   }
   // if connect to database is successful
   else{
 
-    echo "Connected to database ".$db_name;
+    // handle data sent in POST form
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+    // search for user with given credentials
+    $sql = "SELECT * FROM users WHERE email='$login' AND password='$password'";
+
+    // perform query on the database
+    $query_result = @$connection->query($sql);
+    // returns FALSE on failure
+
+    if ($query_result){
+      // how many users are in database with given parametres
+      // it also can return 0!
+      $how_many_users = $query_result->num_rows;
+
+      // if there is at least one such user
+      if ($how_many_users>0){
+        // fetch result row - associative array
+        // save in session variable
+        $_SESSION['user'] = $query_result->fetch_assoc();
+
+        //free the memory associated with the result
+        $query_result->free_result();
+
+        // redirect to logged user's game panel
+        header('Location: game.php');
+      }
+      // if there is no such user
+      else {
+
+        // redirect anonimous user to main site
+        header('Location: index.php');
+      }
+    }
+    // false value means query is wrong
+    else {
+
+    }
+
 
     // close connection
     $connection->close();
